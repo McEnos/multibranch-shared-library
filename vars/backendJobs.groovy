24cb26e8dcccd2a1230197fcc3d23f12ce7d017c
McEnos <JoshuaMcEnos@gmail.com>
2023-11-25 07:44:03 +0300

    Update backendJobs.groovy

--- vars/backendJobs.groovy
+++ vars/backendJobs.groovy
@@ -2,13 +2,13 @@
 
 def call(String submodule = null) {
     if (submodule) {
-        createJob(submodule)
+        buildSubmodule(submodule)
     } else {
-        setupMultiBranchPipeline()
+        echo "No submodule specified. Skipping submodule build."
     }
 }
 
-def createJob(submodule) {
+def buildSubmodule(submodule) {
     pipeline {
         agent any
         triggers {
@@ -23,31 +23,6 @@ def createJob(submodule) {
                 }
             }
             // Add more stages or jobs for each submodule as needed
-        }
-    }
-}
-
-def setupMultiBranchPipeline() {
-    properties([pipelineTriggers([
-            [$class: 'GitHubPushTrigger'],
-    ])])
-
-    triggers {
-        githubPush()
-    }
-
-    branches {
-        branchFilter('*/main') // Adjust the branch filter based on your branch naming
-        //branchFilter('*/feature/*') // Example: include feature branches
-    }
-
-    configure { project ->
-        // Customize multi-branch pipeline configuration if needed
-        project / sources / data /jenkins.branch.BranchSource[sourceOwner="multiBranch"] / traits {
-            gitBranchDiscovery {
-                strategyId(1)
-                trust(Classroom.TRAUST_ALL_CERTIFICATES)
-            }
         }
     }
 }
