85e44dd03634d65ca2fb3038a47a3a7ded0d48e7
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2023-12-02 06:25:57 +0300

    added new backend pipeline

--- src/BackendPipeline.groovy
+++ src/BackendPipeline.groovy
@@ -1,20 +1,50 @@
-def getCiPipeline(){
-    pipeline{
-        agent any
-        stages{
-             stage('Setup') {
-                steps {
-                    script {
-                        echo "Running setups...."
-                    }
+def getCiPipeline() {
+    return {
+        node {
+            def workspaceDir
+            stage('Checkout Code') {
+                checkout scm
+            }
+
+            stage('Unit Test') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo running uni tests in ${workspaceDir}"
                 }
-                stage("Unit test"){
-                    steps{
-                       script {
-                        echo "Running Unit tests..."
-                    } 
-                    }
+            }
+
+            stage('Quality Gate') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo checking  quality in ${workspaceDir}"
+
                 }
+            }
+
+            stage('Set Image Version') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo setting image version"
+                }
+            }
+
+            stage('Set Image Name') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo setting  docker image name in ${workspaceDir}"
+                }
+            }
+
+            stage('Build Docker Image') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo Building docker image in ${workspaceDir}"
+                }
+            }
+
+
+
         }
+
     }
 }
