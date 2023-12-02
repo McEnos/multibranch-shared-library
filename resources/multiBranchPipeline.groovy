9380c747e686fb74681e6162bab444a6703f34bc
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2023-12-02 05:52:23 +0300

    New changes , was using wrong token ID

--- resources/multiBranchPipeline.groovy
+++ resources/multiBranchPipeline.groovy
@@ -76,7 +76,7 @@ def generateMultibranchPipelines(List<Path> jenkinsfilePaths, Path rootFolder, S
                             configuredByUrl(false)
 
                             // Make sure to properly set this.
-                            credentialsId('token_pat')
+                            credentialsId('token-pat')
 
                             traits {
 
