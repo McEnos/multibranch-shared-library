6d165431af050fc140f83d4597dfc35f46f95d09
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2024-02-02 05:40:56 +0300

    Updates

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -2,16 +2,38 @@ def getCiPipeline() {
     return {
         node {
             def workspaceDir
+            def imageName
+            env.JAVA_HOME = tool name: 'java21', type: 'jdk'
             stage('Checkout Code') {
                 checkout scm
             }
-
             stage('Building Docker Image') {
                 workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
                 dir("${env.WORKSPACE}/${workspaceDir}") {
-                    sh "echo compiling and  building latest docker image"
-                    sh "chmod +x mvnw"
-                    sh "./mvnw compile jib:build -DsendCredentialsOverHttp=true"
+                    withCredentials([usernamePassword(credentialsId: 'registry-login', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                        sh "echo \$PASSWORD | docker login -u \$USERNAME --password-stdin localhost:8050"
+                        sh "chmod +x mvnw"
+                        sh "./mvnw jib:build"
+                    }
+                }
+            }
+            stage("Delete image after generation"){
+                sh "echo deleting ${imageName} image"
+                sh "docker rmi ${imageName}"
+            }
+
+
+            stage('Quality Gate') {
+                workspaceDir = sh(script: 'ls -d */|head -n 1', returnStdout: true).trim()
+                dir("${env.WORKSPACE}/${workspaceDir}") {
+                    sh "echo checking  quality in ${workspaceDir}"
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    sh "docker logout"
                 }
             }
         }
